6a2b9e882f3c80bcc8520fb30e7c0d55aa17df24
Juan Bertinetti <juanbertinetti@gmail.com>
2020-10-27 17:54:46 -0300

    Separate logic in parser class with different parser subclasses

--- gst.py
+++ gst.py
@@ -1,61 +1,93 @@
 #!/bin/env python3
 
 import os
+import pathlib
 import subprocess
 import sys
 
-def get_status():
-    """git status"""
-    p = subprocess.run(["git", "status", "--porcelain", "--no-renames"], capture_output=True, text=True)
-    res = [l for l in p.stdout.split("\n") if l]
-    return res
+class Parser:
+    def __init__(self):
+        self.st = self.get_status()
 
-def print_status(st):
-    width = len(str(len(st)-1))
-    for i, f in enumerate(st):
-        print("{i:{w}d}:".format(i=i, w=width), f)
+    def execute(self, subcmd, ops):
+        idx = self.parse_input(ops)
+        files = self.parse_status(idx)
+        cmd = self.get_cmd() + subcmd + files
+        subprocess.run(cmd)
 
-def parse_input(input_string):
-    if not input_string.replace(" ", "").replace("-", "").isnumeric():
-        raise Exception("Invalid input.")
-    idx = set()
-    for t in input_string.split():
-        if "-" in t:
-            tt = t.split("-")
-            if len(tt) != 2 or not tt[0] or not tt[1]:
-                raise Exception("Invalid input.")
-            a, b = int(tt[0]), int(tt[1])
-            a, b = (a, b) if a < b else (b, a)
-            idx.update(range(a, b+1))
-        else:
-            idx.add(int(t))
-    # return sorted(list(idx))
-    return idx
+    def get_cmd(self) -> list:
+        raise NotImplementedError
 
-def parse_status(st, idx):
-    if max(idx) > len(st) - 1:
-        raise Exception("Invalid input.")
-    files = [parse_status_line(st[i]) for i in idx]
-    return files
+    def get_status(self):
+        raise NotImplementedError
 
-def parse_status_line(line):
-    line = line[3:]
-    return line[1:-1] if line.startswith('"') and line.endswith('"') else line
+    def print_status(self):
+        width = len(str(len(self.st)-1))
+        for i, f in enumerate(self.st):
+            print("{i:{w}d}:".format(i=i, w=width), f)
+
+    def parse_input(self, input_string):
+        if not input_string.replace(" ", "").replace("-", "").isnumeric():
+            raise Exception("Invalid input.")
+        idx = set()
+        for t in input_string.split():
+            if "-" in t:
+                tt = t.split("-")
+                if len(tt) != 2 or not tt[0] or not tt[1]:
+                    raise Exception("Invalid input.")
+                a, b = int(tt[0]), int(tt[1])
+                a, b = (a, b) if a < b else (b, a)
+                idx.update(range(a, b+1))
+            else:
+                idx.add(int(t))
+        # return sorted(list(idx))
+        return idx
+
+    def parse_status(self, idx):
+        if max(idx) > len(self.st) - 1:
+            raise Exception("Invalid input.")
+        files = [self.parse_status_line(self.st[i]) for i in idx]
+        return files
+
+    def parse_status_line(self, line):
+        raise NotImplementedError
+
+
+class GitParser(Parser):
+    def get_status(self):
+        """git status"""
+        p = subprocess.run(["git", "status", "--porcelain", "--no-renames"], capture_output=True, text=True)
+        res = [l for l in p.stdout.split("\n") if l]
+        return res
+
+    def parse_status_line(self, line):
+        line = line[3:]
+        return line[1:-1] if line.startswith('"') and line.endswith('"') else line
+
+    def get_cmd(self):
+        return ["git"]
+
+
+class LsParser(Parser):
+    def get_status(self):
+        p = pathlib.Path()
+        return list(p.iterdir())
+
+    def parse_status_line(self, line):
+        return line
+
+    def get_cmd(self):
+        return []
 
 
 if __name__ == "__main__":
-    st = get_status()
+    p = LsParser()
     if len(sys.argv) == 1:
-        print_status(st)
+        p.print_status()
     else:
         print("Select files:")
-        print_status(st)
+        p.print_status()
         ops = input("Enter: ")
-        ops = parse_input(ops)
+        p.execute(sys.argv[1:], ops)
 
-        files = parse_status(st, ops)
-        cmd = ["git"] + sys.argv[1:] + files
-        # cmd = "git {} -- {}".format(" ".join(sys.argv[1:]), " ".join(files))
-
-        subprocess.run(cmd)
     # print(os.getcwd())
